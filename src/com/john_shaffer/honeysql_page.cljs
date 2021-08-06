a4a293b0dff72ac1f6af2e9ba645b3d974cae281
John Shaffer <jshaffer2112@protonmail.com>
2021-08-05 21:19:39 -0500

    Get initial state from query params. Set URI on state change.

--- src/com/john_shaffer/honeysql_page.cljs
+++ src/com/john_shaffer/honeysql_page.cljs
@@ -46,10 +46,22 @@
          (pr-str (vec more))))]))
 
 (defn App []
-  (let [state (r/atom {:options default-options
-                       :query-map default-query-map})]
+  (let [params (try ; Don't die on old browsers
+                 (js/URLSearchParams. js/window.location.search)
+                 (catch js/Error _))
+        get-param #(if params (or (.get params %) %2) %2)
+        state (r/atom {:options (get-param "opt" default-options)
+                       :query-map (get-param "q" default-query-map)})]
     (fn []
       (let [{:keys [options query-map]} @state]
+        (try ; Don't die on old browsers
+          (js/window.history.replaceState nil ""
+            (if (and (= options default-options)
+                  (= query-map default-query-map))
+              "."
+              (str "?q=" (js/encodeURIComponent query-map)
+                "&opt=" (js/encodeURIComponent options))))
+          (catch js/Error _))
         [:div
          [:span
           [:a {:href "https://github.com/seancorfield/honeysql"}
