027a74527a8f486068c4e67b4ebfaa6c261bc651
John Shaffer <jshaffer2112@protonmail.com>
2021-08-05 20:14:59 -0500

    Add deploy.sh.

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+set -e
+
+shadow-cljs release main
+aws s3 cp public/honeysql-page/js/main.js s3://john-shaffer-com/honeysql-page/js/main.js
+aws s3 sync public/honeysql-page/css s3://john-shaffer-com/honeysql-page/css
+aws s3 cp public/index.html s3://john-shaffer-com/honeysql/index.html
+aws cloudfront create-invalidation --distribution-id EAB0WURMU6NJY --paths "/honeysql-page/*" "/honeysql/*"
