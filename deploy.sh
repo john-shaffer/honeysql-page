62309d0fe7ab56909140bc7428dfb2e0ea213d8a
John Shaffer <jdsha@pm.me>
2025-09-30 13:33:58 -0500

    Fix default should be the latest version
    Since the oldest version of HoneySQL is built
    last, the default page was using that version.
    Fix by finding the newest version and copying it
    into public/ before deploying.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 set -euxo pipefail
 
 ./build.clj release
+LATEST_VERSION=v$(jq -r .[0] < public/versions.edn)
+echo "Deploying $LATEST_VERSION"
+cp -rv public/"$LATEST_VERSION"/* public
 aws s3 sync public/honeysql-page/css s3://john-shaffer-com/honeysql-page/css
 aws s3 cp public/js/main.js s3://john-shaffer-com/honeysql/js/main.js
 aws s3 sync public s3://john-shaffer-com/honeysql --exclude "honeysql-page/*" --exclude "js/*"
